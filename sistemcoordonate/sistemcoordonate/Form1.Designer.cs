60f4fda51a0ea56168f184b3df31e50baaab7340
theguzg <53229654+theguzg@users.noreply.github.com>
2019-07-23 19:42:18 +0300

    Add files via upload

---
+++ sistemcoordonate/sistemcoordonate/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace sistemcoordonate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.xPoint1 = new System.Windows.Forms.TextBox();
+            this.xPoint2 = new System.Windows.Forms.TextBox();
+            this.yPoint2 = new System.Windows.Forms.TextBox();
+            this.yPoint1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.mouseStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw Line";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Point 1 --->";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Point 2 --->";
+            // 
+            // xPoint1
+            // 
+            this.xPoint1.Location = new System.Drawing.Point(107, 120);
+            this.xPoint1.Name = "xPoint1";
+            this.xPoint1.Size = new System.Drawing.Size(46, 20);
+            this.xPoint1.TabIndex = 3;
+            this.xPoint1.TextChanged += new System.EventHandler(this.xPoint1_TextChanged);
+            // 
+            // xPoint2
+            // 
+            this.xPoint2.Location = new System.Drawing.Point(107, 154);
+            this.xPoint2.Name = "xPoint2";
+            this.xPoint2.Size = new System.Drawing.Size(46, 20);
+            this.xPoint2.TabIndex = 3;
+            this.xPoint2.TextChanged += new System.EventHandler(this.xPoint2_TextChanged);
+            // 
+            // yPoint2
+            // 
+            this.yPoint2.Location = new System.Drawing.Point(181, 154);
+            this.yPoint2.Name = "yPoint2";
+            this.yPoint2.Size = new System.Drawing.Size(46, 20);
+            this.yPoint2.TabIndex = 3;
+            this.yPoint2.TextChanged += new System.EventHandler(this.yPoint2_TextChanged);
+            // 
+            // yPoint1
+            // 
+            this.yPoint1.Location = new System.Drawing.Point(181, 120);
+            this.yPoint1.Name = "yPoint1";
+            this.yPoint1.Size = new System.Drawing.Size(46, 20);
+            this.yPoint1.TabIndex = 3;
+            this.yPoint1.TextChanged += new System.EventHandler(this.yPoint1_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mouseStatus,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 565);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(901, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // mouseStatus
+            // 
+            this.mouseStatus.Name = "mouseStatus";
+            this.mouseStatus.Size = new System.Drawing.Size(46, 17);
+            this.mouseStatus.Text = "X: - Y: -";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "X:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "X:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(158, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Y:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(159, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Y:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 587);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.yPoint2);
+            this.Controls.Add(this.xPoint2);
+            this.Controls.Add(this.yPoint1);
+            this.Controls.Add(this.xPoint1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox xPoint1;
+        private System.Windows.Forms.TextBox xPoint2;
+        private System.Windows.Forms.TextBox yPoint2;
+        private System.Windows.Forms.TextBox yPoint1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel mouseStatus;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ sistemcoordonate/sistemcoordonate/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistemcoordonate
+{
+    public partial class Form1 : Form
+    {
+        Graphics g; Pen p; Point cursor; Rectangle rect; SolidBrush whiteBrush; Point point1; Point point2;
+        private bool button1IsClicked = false;
+
+        int x1 = -1; int y1 = -1; int x2 = -1; int y2 = -1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            p = new Pen(Color.Black, 2);
+            rect = new Rectangle(300, 150, 300, 300);
+            g = this.CreateGraphics();
+            whiteBrush = new SolidBrush(Color.White);
+
+            
+            point1.X = x1;
+            point1.Y = y1;
+
+            point2.X = x2;
+            point2.Y = y2;
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+
+            button1IsClicked = true;
+
+            if (!(String.IsNullOrWhiteSpace(xPoint1.Text)))
+                x1 = Int32.Parse(xPoint1.Text);
+
+            if (!(String.IsNullOrWhiteSpace(yPoint1.Text)))
+                y1 = Int32.Parse(yPoint1.Text);
+
+            if (!(String.IsNullOrWhiteSpace(xPoint2.Text)))
+                x2 = Int32.Parse(xPoint2.Text);
+
+            if (!(String.IsNullOrWhiteSpace(yPoint2.Text)))
+                y2 = Int32.Parse(yPoint2.Text);
+
+            if (x1 >= 0 && x1 <= 100 && y1 >= 0 && y1 <= 100 && x2 >= 0 && x2 <= 100 && y2 >= 0 && y2 <= 100 )
+                g.DrawLine(p, x1 * 3 + 300, y1 + 450 - (y1 * 4), x2 * 3 + 300, y2 + 450 - (y2 * 4));
+
+        }
+
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cursor = this.PointToClient(Cursor.Position);
+            mouseStatus.Text = "X: " + ((cursor.X - 300) / 3) + "Y: " + ((cursor.Y - 450) / 3 * (-1));
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g.FillRectangle(whiteBrush, rect);
+            g.DrawRectangle(p, rect);
+
+            if (button1IsClicked == true)
+            {
+                if (!(String.IsNullOrWhiteSpace(xPoint1.Text)))
+                    x1 = Int32.Parse(xPoint1.Text);
+
+                if (!(String.IsNullOrWhiteSpace(yPoint1.Text)))
+                    y1 = Int32.Parse(yPoint1.Text);
+
+                if (!(String.IsNullOrWhiteSpace(xPoint2.Text)))
+                    x2 = Int32.Parse(xPoint2.Text);
+
+                if (!(String.IsNullOrWhiteSpace(yPoint2.Text)))
+                    y2 = Int32.Parse(yPoint2.Text);
+
+                
+                if (x1 >= 0 && x1 <= 100 && y1 >= 0 && y1 <= 100 && x2 >= 0 && x2 <= 100 && y2 >= 0 && y2 <= 100)
+                    g.DrawLine(p, x1 * 3 + 300, y1 + 450 - (y1 * 4), x2 * 3 + 300, y2 + 450 - (y2 * 4));
+
+                
+            }
+
+            
+
+        }
+
+        private void xPoint1_TextChanged(object sender, EventArgs e)
+        {
+            button1IsClicked = false;
+        }
+
+        private void yPoint1_TextChanged(object sender, EventArgs e)
+        {
+            button1IsClicked = false;
+        }
+
+        private void xPoint2_TextChanged(object sender, EventArgs e)
+        {
+            button1IsClicked = false;
+        }
+
+        private void yPoint2_TextChanged(object sender, EventArgs e)
+        {
+            button1IsClicked = false;
+        }
+    }
+
+}
