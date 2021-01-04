919872d1b589cfa040e6104bc6c5c01c418130e2
theguzg <53229654+theguzg@users.noreply.github.com>
2021-01-04 13:42:31 +0200

    larevedere

--- licenta/angularjwtauth/src/app/human-body/human-body.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-human-body',
-  templateUrl: './human-body.component.html',
-  styleUrls: ['./human-body.component.css']
-})
-export class HumanBodyComponent {
-}
